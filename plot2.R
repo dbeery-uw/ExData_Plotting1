4c0962a409b0fd4889f153900242c86d78c5d025
dbeery-uw <dbeery24@uw.edu>
2022-09-17 12:20:15 -0700

    R code for plot2.png

---
+++ plot2.R
@@ -0,0 +1,24 @@
+##Plot 2
+library(tidyr)
+library(dplyr)
+library(ggplot2)
+library(stringr)
+library(lubridate)
+
+household <- read.table("household_power_consumption.txt", sep = ";", header = TRUE)
+
+tail(household$Date)
+twoday <- household %>%
+  filter(Date == c("1/2/2007", "2/2/2007"))
+
+twoday[,3:9] <- sapply(twoday[,3:9], as.numeric)
+twoday$date_time <- str_c(twoday$Date," " , twoday$Time)
+twoday$date_time <- dmy_hms(twoday$date_time)
+
+
+png(file = "plot2.png")
+plot(twoday$date_time, twoday$Global_active_power, 
+     type = "l",
+     xlab = " ",
+     ylab = "Global Active Power (kilowatts)")
+dev.off()
